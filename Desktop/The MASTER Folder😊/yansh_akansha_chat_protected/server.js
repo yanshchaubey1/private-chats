731c0bca9a2e5de7c8818d6211d57c08707c2423
yansh chaubey <snchaubey1982@gmail.com>
2025-08-05 15:28:00 +0530

    fix: added static file serving

---
+++ Desktop/The MASTER Folder😊/yansh_akansha_chat_protected/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const path = require('path');
+
+// Serve static files from "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Serve index.html when / is requested
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
